b968ee34de6f71d55064ff0bc636b109050ad9a3
Lyuboslav Karev <lyubo.karev@gmail.com>
2024-02-16 17:11:41 +0200

    Added the CodeSnippet class

---
+++ src/code_snippet.py
@@ -0,0 +1,29 @@
+"""
+Module containing a model for the code snippet
+"""
+
+from typing import Optional
+
+
+class CodeSnippet:
+    def __init__(self, name: str, code: str, language: str, description: Optional[str] = None):
+        self.__name = name
+        self.__code = code
+        self.__language = language
+        self.__description = description
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def code(self) -> str:
+        return self.__code
+
+    @property
+    def language(self) -> str:
+        return self.__language
+
+    @property
+    def description(self) -> Optional[str]:
+        return self.__description
