eae8c02c681a4dd794b33d0c9204568337dad163
Lyuboslav Karev <lyubo.karev@gmail.com>
2024-02-18 11:42:02 +0200

    Added docstrings to code_snippet

--- src/code_snippet.py
+++ src/code_snippet.py
@@ -6,6 +6,10 @@ from typing import Optional
 
 
 class CodeSnippet:
+    """
+    Class to model a snippet of code. 
+    Contains the name of the snippet, the code, the language and optional description.
+    """
     def __init__(self, name: str, code: str, language: str, description: Optional[str] = None):
         self.__name = name
         self.__code = code[1:-1]  # Remove the start and end of the codeblock
@@ -14,16 +18,32 @@ class CodeSnippet:
 
     @property
     def name(self) -> str:
+        """
+        Returns the name of the snippet
+        :rtype: str
+        """
         return self.__name
 
     @property
     def code(self) -> str:
+        """
+        Returns the code of the snippet
+        :rtype: str
+        """
         return self.__code
 
     @property
     def language(self) -> str:
+        """
+        Returns the language of the snippets
+        :rtype: str
+        """
         return self.__language
 
     @property
     def description(self) -> Optional[str]:
+        """
+        Returns the description of the snippet
+        :rtype: Optional[str]
+        """
         return self.__description
