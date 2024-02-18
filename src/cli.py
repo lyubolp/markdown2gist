99b2609ec7bd7eab06558e27d95f52ac139bae98
Lyuboslav Karev <lyubo.karev@gmail.com>
2024-02-18 12:53:50 +0200

    Added docstrings to cli

--- src/cli.py
+++ src/cli.py
@@ -3,9 +3,16 @@ Module containing the CLI interface of the app.
 """
 
 from argparse import ArgumentParser
+from typing import Any
 
 
 def setup_cli() -> ArgumentParser:
+    """
+    Sets up the command line interface
+
+    :return: The CLI parser
+    :rtype: ArgumentParser
+    """
     parser = ArgumentParser(prog='markdown2gist',
                             description='Convert code snippets from markdown into gists')
 
@@ -14,5 +21,13 @@ def setup_cli() -> ArgumentParser:
     return parser
 
 
-def parse_args(parser: ArgumentParser):
+def parse_args(parser: ArgumentParser) -> dict[str, Any]:
+    """
+    Parses and returns the CLI arguments as a dictionary
+
+    :param parser: The argument parser object
+    :type parser: ArgumentParser
+    :return: Dictionary containing the arguments
+    :rtype: Dictionary
+    """
     return parser.parse_args().__dict__
